8a7e05606d6657ad4d694fc4903a0b579c52f239
UzuiTengen117 <juanca.chaparro.18@gmail.com>
2024-02-23 07:27:17 -0600

    Cerrar venta cuando selecionas ir a una ventana

---
+++ src/areglos/pkg4/Areglos4.java
@@ -0,0 +1,16 @@
+package areglos.pkg4;
+
+public class Areglos4 {
+
+    public static void main(String[] args) {
+
+        int[] Viernes = {7, 20, 41, 19, 14, 32};
+
+        for (int i = 0; i < Viernes.length; i++) 
+        {
+            System.out.println(Viernes[i]);
+        }
+
+    }
+
+}
